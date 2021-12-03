a41f5bb74caa7d1377784dc80ddbf5620d70357b
Seigo Kawamura <seigo.kawamura@gmail.com>
2021-12-03 10:59:36 +0900

    delete absolute path from commnad execution

--- airpods.sh
+++ airpods.sh
@@ -11,14 +11,14 @@ echo $AP_ADDRESS
 echo $AP_NAME
 
 #BT connection
-/usr/local/bin/bluetoothconnector -c $AP_ADDRESS
+bluetoothconnector -c $AP_ADDRESS
 
 # Trying to switch audio source
 for ((i=0 ; i<10 ; i++))
 do
-  if [ 'Connected' == $(/usr/local/bin/bluetoothconnector -s $AP_ADDRESS) ]; then
+  if [ 'Connected' == $(bluetoothconnector -s $AP_ADDRESS) ]; then
     sleep 1
-    /usr/local/Cellar/switchaudio-osx/1.1.0/SwitchAudioSource -s "$AP_NAME"
+    SwitchAudioSource -s "$AP_NAME"
     sleep 1
     say -v samantha Connected
     break
